1b7948ec273753250c032e879a3bafbc0a9eb0a3
FBeaumont <florianbeaumont412@gmail.com>
2018-06-25 16:32:29 +0200

    Core location training

---
+++ UVCare/UVCare/Model/WeatherDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataModel.swift
+//  UVCare
+//
+//  Created by Florian on 6/23/18.
+//  Copyright © 2018 com.Florian. All rights reserved.
+//
+
+import Foundation
---
+++ trainingGeoLocal/trainingGeoLocal/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  trainingGeoLocal
+//
+//  Created by Florian on 6/24/18.
+//  Copyright © 2018 com.Florian. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var localisationLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // If location services is enabled get the users location
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    
+    // Print out the location to the console
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print(location.coordinate)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    
+    // If we have been deined access give the user the option to change it
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            showLocationDisablePopUp()
+        }
+    }
+    
+    
+    // Show the popup to user if the location access was denied
+    func showLocationDisablePopUp() {
+        let alertController = UIAlertController(title: "Background Location Access Disabled",
+                                                message: "We need your Location",
+                                                preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+
+
+        let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+            if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
